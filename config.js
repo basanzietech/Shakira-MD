1d862e7f6281e2db91e0e7ec783209c263ca25b5
basanzietech <167584894+basanzietech@users.noreply.github.com>
2024-05-05 22:24:23 +0300

    Update config.js

--- config.js
+++ config.js
@@ -92,7 +92,7 @@ module.exports = {
 
 
   WORKTYPE: process.env.WORKTYPE || process.env.MODE|| "private",
-  LANG: ( process.env.THEME ||  "MILES"  ).toUpperCase(),
+  LANG: ( process.env.THEME ||  "SHAKIRA"  ).toUpperCase(),
 
 
 
